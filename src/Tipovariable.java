a010c8e8f67279430794040c8fade599655f676f
Juan Pablo <jkalemke@gmail.com>
2023-04-26 15:17:13 -0300

    first commit

---
+++ src/Tipovariable.java
@@ -0,0 +1,10 @@
+
+public class Tipovariable {
+	
+	public static void main(String[] args) {
+		double variable1 = 230.89;
+		int variable1Entero = (int) variable1; //cast
+		
+		System.out.println(variable1Entero);
+	}
+}
